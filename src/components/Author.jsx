f12f428ed7a94080bf18bf2128c36ba613127d1c
aayushchauhan10 <aayushchauhanrajput47@gmail.com>
2023-12-16 10:46:41 +0530

    Commiting for the 3rd time

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -1,10 +1,6 @@
 import React, { useState } from "react";
 import { mainVideo, playVideo } from "../assets/images";
 
-{
-  /* <iframe width="560" height="315" src="https://www.youtube.com/embed/n7Nn9t8dyiU?si=ha_cVme4xMJRbS7a" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */
-}
-
 const Author = () => {
   const youtubeVideoURL =
     "https://www.youtube.com/embed/n7Nn9t8dyiU?si=ha_cVme4xMJRbS7a";
